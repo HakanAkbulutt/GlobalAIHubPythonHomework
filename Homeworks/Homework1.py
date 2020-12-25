3b7b4c92f61a7a1b89879b26da39f6e30b617530
Hakan <buff0710@gmail.com>
2020-12-25 11:16:56 +0300

    ilk dosyalarim

---
+++ Homeworks/Homework1.py
@@ -0,0 +1,13 @@
+a= str(input("Enter a string value a: "))
+b= int(input("Enter an integer value b: "))
+c= float(input("Enter a float value c: "))
+d= bool(input("Enter a boolean  value d: "))
+e= complex(input("Enter a complex value e: "))
+
+print ("The class of the value : {}".format(type(a)))
+print ("The class of the value : {}".format(type(b)))
+print ("The class of the value : {}".format(type(c)))
+print ("The class of the value : {}".format(type(d)))
+print ("The class of the value : {}".format(type(e)))
+
+
